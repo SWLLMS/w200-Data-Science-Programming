b9559684a14670399ce43ad6bca290681658b6d5
SWLLMS <56372576+SWLLMS@users.noreply.github.com>
2020-08-10 11:22:11 -0400

    w200 Week 1
    MIDS w200 Spring 2020

---
+++ assignments/week-1/make_tree.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p ./Documents/Berkeley/DS200/Sam_WilliamsREPO/week_01/s1/{s3,s2/Advanced}
+echo "I love bash scripting." > ./Documents/Berkeley/DS200/Sam_WilliamsREPO/week_01/s1/s3/conf.txt
+echo "A whole new world" "A new fantastic point of view" > ./Documents/Berkeley/DS200/Sam_WilliamsREPO/week_01/s1/s2/text_chunk1.txt
+cp ./Documents/Berkeley/DS200/Sam_WilliamsREPO/week_01/s1/s2/text_chunk1.txt ./Documents/Berkeley/DS200/Sam_WilliamsREPO/week_01/s1/s2/Advanced/text_chunk2.txt
+echo "Do you want to build a snowman?" >> ./Documents/Berkeley/DS200/Sam_WilliamsREPO/week_01/s1/s2/Advanced/text_chunk2.txt
